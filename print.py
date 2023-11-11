9767a5d27b6625287ed73d79f96d9cb51ffb79c1
MrMac4410 <143443877+MrMac4410@users.noreply.github.com>
2023-11-11 07:34:28 -0800

    Update print.py

--- print.py
+++ print.py
@@ -1,1 +1,7 @@
+# Sending information to the terminal is one of the first things you will learn in any codeing language.
+# the iconic "Hello, World!" is always the first thing you do. The method used in python to send this
+# information to your terminal is Print() 
+
+print("Hello, World!")
+
 
