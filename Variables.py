5fc40a8f3351ab6275e05badb872ca1d353e8bd6
Patrick McNamee <mcnamee4410@gmail.com>
2023-11-13 09:06:35 -0800

    updating changes

---
+++ Variables.py
@@ -0,0 +1,15 @@
+# Variables are containers that hold informaiton,  you can assign
+# numbers or intigers as well as strings to a variable.
+# then call the variable instead of re-typing the same thing over
+# and over again.  
+
+first_string_var = "Hello, World!"
+first_integer_var = 2
+
+# When you add a string to a variable then make sure you use qoutes.
+# intigers and nubmers do not need qoutes. use the print() to call 
+# the varable to the terminal. 
+
+print(first_integer_var)
+print(first_integer_var)
+
