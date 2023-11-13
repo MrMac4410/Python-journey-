13798f0cd28682c09cdd4c641e223c0f77c79344
Patrick McNamee <mcnamee4410@gmail.com>
2023-11-13 09:33:41 -0800

    updating

--- Variables.py
+++ Variables.py
@@ -10,6 +10,30 @@ first_integer_var = 2
 # intigers and nubmers do not need qoutes. use the print() to call 
 # the varable to the terminal. 
 
-print(first_integer_var)
+print(first_string_var)
 print(first_integer_var)
 
+# You should see Hello, World! and the number 2 in your terminal.  
+# Variables can be simple one lines or multi lines of text, arrays,
+# or intigers.  
+
+# to add multi line of text form a qoute or story from something you read
+# you will use the triple qoute function
+
+first_multi_string_var = """
+The good news about comptuers is 
+that they do what you tell them to.
+The bad news is that they do what 
+you tell them too. 
+'Ted Nelson'
+"""
+
+# Now if i print() first_multi_string_var it will send the qoute
+# and match the formating as best as it can.  
+
+print(first_multi_string_var)
+
+
+# Notice in all these variable examples i do not use qoutes to call
+# the variable as they are not requried.  you simply type the variable
+# name into the print() brackets. 
