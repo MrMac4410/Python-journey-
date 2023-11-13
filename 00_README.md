299c453a524cee75ea503bba49c38027c495b337
Patrick McNamee <mcnamee4410@gmail.com>
2023-11-13 11:17:04 -0800

    updated print()

--- 00_README.md
+++ 00_README.md
@@ -3,23 +3,32 @@
 This is my journey to learning Python as well as GitHub. 
 I have always had difficulty learning codeing languages of any kind. 
 I found that I retain information alot better if I write out what im learning in my own words and in a way that make sense to me.  
-I welcome any critisizm of what I have written or help if I may have written something incorrectly. 
 
-I am going to add each lesson as its own .py in the file tree with their own comments and combine what I think belongs together as 
-its becomes more complex and bloated. I am sure there will be alot of unnecessary files I will have to re-organize as I get more
-proficient with GitHub and Python. 
+I welcome any critisizm of what I have written or help if I may have written something incorrectly. There is always a better way. 
+
+I am going to add each lesson as its own .py in the file tree with their own comments and combine what I think belongs together
+as it becomes more complex and bloated. 
+
+I am sure there will be alot of unnecessary files I will have to re-organize as I get more proficient with GitHub and Python. 
 
 
 Connecting visual studio to my GitHub. 
 
 Using Visual Studio code is easy for me to use as it provides alot of extensions and organization. 
-after a quick google search the fastest way i found to connect my GitHub to my Visual Studio was to follow 
-these instructions 
+
+After a quick google search the fastest way I found to connect my GitHub to my Visual Studio was to follow these instructions 
 
 https://learn.microsoft.com/en-us/azure/developer/javascript/how-to/with-visual-studio-code/clone-github-repository?tabs=create-repo-command-palette%2Cinitialize-repo-activity-bar%2Ccreate-branch-command-palette%2Ccommit-changes-command-palette%2Cpush-command-palette
 
 There has to be a better way to make links shorter, I will have to look it up later. 
 
 
-Some of the extensions i have installed in Visual Studio are pylance, python, python enviornment manager, python extension pack and python indent. 
+Some of the extensions i have installed in Visual Studio are:
+pylance, 
+python, 
+python enviornment manager, 
+python extension pack and 
+python indent. 
+
+
 
--- 01_print.py
+++ 01_print.py
@@ -1,12 +1,31 @@
-# Sending information to the terminal is one of the first things you will learn in any codeing language.
-# the iconic "Hello, World!" is always the first thing you do. The method used in python to send this
-# information to your terminal is print() 
+# Sending information to the terminal is one of the first things you will learn
+# in any codeing language. The iconic "Hello, World!" is always one of first things
+# you do. The method used in python to send this information to your terminal is print() 
 
 print("Hello, World!")
 
-# Hello, World! is considered a string, a string is a sequence of characters "a", "e", "i", "o", "u". When you add a string you should
-# always put it in Double "" or single '' qoutes.   Double qoutes are more commenly used in python, as it simplifies some of the issues
-# you can have when you need to use a apostrophe in a sentence. "There, Their and They're".  if you use sing qoutes 'There, Their, and They're'.
-# it would assume the string ended at 'There, Their, and they'.   The use of double or single qoutes is interchangable,  but should be consistent throughout
-# your codeing.  
+# Hello, World! is considered a string, a string is a sequence of characters
+# "a", "e", "i", "o", "u". When you add a string you should always put it 
+# in Double "" or single '' qoutes. Double qoutes are more commenly used in 
+# python, as it simplifies some of the issues you can have when you need to 
+# use a apostrophe in a sentence. "There, Their and They're". 
+
+# print('There, Their, They're')
+      
+# if you use single qoutes 'There, Their, and They're'. it would assume the 
+# string ended at 'There, Their, and they'. and throw and error stating
+# 're' was not closed or was missing a single qoute mark '. You could use triple qoutes
+# if you really want to use the single qoute.  
+
+print('''There, Their, They're''') 
+
+# But from what ive read so far I beleive this to me a mis-use of the triple qoutes and
+# does not follow standard coding guidance.  
+
+# it would be simplier in my opinion to use double qoutes "".
+
+print("There, Their, They're")
+
+
+# The use of double or single qoutes is interchangable, but should be consistent throughout your codeing.  
 
